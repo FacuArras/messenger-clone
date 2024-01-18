c3909f9fc0beb32a3cb2d721fe563dfe6dff0f21
facundo <facundoarrascaetacba@gmail.com>
2024-01-17 23:26:08 -0300

    problem fixed

--- next.config.js
+++ next.config.js
@@ -12,9 +12,6 @@ const nextConfig = {
             "avatars.githubusercontent.com",
             "lh3.googleusercontent.com"
         ]
-    },
-    typescript: {
-        ignoreBuildErrors: true
     }
 }
 
